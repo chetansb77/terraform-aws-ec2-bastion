cb475d71f36977fcf91dec0dc81f18ecc4c9ad16
Chetan Bhat <Chetan_Bhat@mckinsey.com>
2024-12-09 20:36:30 +0530

    (fix): typo

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "security_group_ingress_cidr_block" {
   description = "CIDR block for allowed SSH & HTTP access"
   type        = string
-  default     = "<<enter you local public ip to whitelist>>"
+  default     = "<<enter your local public ip to whitelist>>"
 }
 
 variable "key_name" {
